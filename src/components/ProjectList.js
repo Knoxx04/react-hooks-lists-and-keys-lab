c60ccd3970004757b118d8a3ad16441d1bca566e
Knoxx04 <onyiknoxx3@gmail.com>
2023-01-13 07:01:16 +0300

    completed assignment

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,14 +1,29 @@
 import React from "react";
 import ProjectItem from "./ProjectItem";
 
-function ProjectList({ projects }) {
+function ProjectList({projects}) {
   console.log(projects);
+  const ProjectItemlists=projects.map((project)=> {
+    return <ProjectItem
+      key={project.id}
+      name={project.name}
+      about={project.about}
+      technologies={project.technologies}
+      />;
+    });
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {ProjectItemlists}
+      </div>
     </div>
   );
 }
 
 export default ProjectList;
+
+
+
+
+ 
